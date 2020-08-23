173bb130133b4d20a3a9d57f2e4a44234f2a0828
Anatoly Tsyganenko <github@h-tol.net>
2020-08-23 03:59:26 +0300

    typos and adding venv

--- scripts/pkgs.sh
+++ scripts/pkgs.sh
@@ -2,4 +2,5 @@ apt update
 apt install -y mc neovim build-essential libssl-dev zlib1g-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
     xz-utils tk-dev libffi-dev liblzma-dev python-openssl git mtr-tiny \
-    zsh fish
+    zsh fish && \
+    python3-venv
