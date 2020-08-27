ad768db703b00d834a2d89104af0b7de49209a85
Anatoly Tsyganenko <github@h-tol.net>
2020-08-27 13:55:26 +0300

    change provisioning priorities. fix pkgs typos

--- scripts/pkgs.sh
+++ scripts/pkgs.sh
@@ -2,5 +2,5 @@ apt update
 apt install -y mc neovim build-essential libssl-dev zlib1g-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
     xz-utils tk-dev libffi-dev liblzma-dev python-openssl git mtr-tiny \
-    zsh fish fzf ripgrep && \
+    zsh fish fzf ripgrep \
     python3-venv
