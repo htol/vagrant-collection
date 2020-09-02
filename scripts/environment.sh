633ed1d1b98c438461d9a1af64f205c53ea51f00
Anatoly Tsyganenko <github@h-tol.net>
2020-09-02 14:59:31 +0300

    provision from github script

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,5 +1,2 @@
 sudo su -l $USERNAME -c " \
-    git clone https://github.com/TheLocehiliosan/yadm.git ~/.local/yadm-project && \
-    mkdir -p ~/.local/bin && \
-    ln -s ~/.local/yadm-project/yadm ~/.local/bin/yadm && \
-    ~/.local/bin/yadm clone --no-bootstrap https://github.com/htol/dotfiles.git"
+    curl -fsSL https://raw.githubusercontent.com/htol/dotfiles/master/.local/bin/deploy_yadm.sh | bash"
