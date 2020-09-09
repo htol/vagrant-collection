661e44009351655368235b5db60eabe6776e61a1
Anatoly Tsyganenko <github@h-tol.net>
2020-09-09 15:08:11 +0300

    adding packages

--- scripts/pkgs.sh
+++ scripts/pkgs.sh
@@ -4,4 +4,4 @@ apt install -y mc neovim build-essential libssl-dev zlib1g-dev libbz2-dev \
     xz-utils tk-dev libffi-dev liblzma-dev python-openssl git mtr-tiny \
     zsh fish fzf ripgrep \
     python3-venv \
-    ssh-import-id tmux openconnect
+    sshpass ssh-import-id tmux openconnect
